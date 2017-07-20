9066ef3ad9543af9868ed67601d6c85099b285fd
Grigory Panov <gripanov@gmail.com>
2017-07-20 11:58:36 +0300

    TSlint was added.

--- src/teamcityapi/tcapiprovider.ts
+++ src/teamcityapi/tcapiprovider.ts
@@ -14,16 +14,15 @@ export interface TCApiProvider {
 
 export class TCRestApiProvider implements TCApiProvider {
     /**
-     * 
-     * @param method - type of request (GET, POST, ...) 
-     * @param url - url of request 
+     * @param method - type of request (GET, POST, ...)
+     * @param url - url of request
      * @param cred - Credential for basic authorization
-     * @return Promise with request.response in case of success, otherwise a reject with status of response and statusText.  
+     * @return Promise with request.response in case of success, otherwise a reject with status of response and statusText.
      */
     private makeRequest(method, url, cred : Credential) {
         const XMLHttpRequest = XHR.XMLHttpRequest;
         return new Promise(function (resolve, reject) {
-            var request : XHR.XMLHttpRequest = new XMLHttpRequest();
+            const request : XHR.XMLHttpRequest = new XMLHttpRequest();
             request.open(method, url, true);
             request.setRequestHeader("Authorization", "Basic " + new Buffer(cred.user + ":" + cred.pass).toString("base64"));
             request.onload = function () {
@@ -47,33 +46,30 @@ export class TCRestApiProvider implements TCApiProvider {
     }
 
     /**
-     * 
      * @param cred Credential of user
-     * @return Promise<boolean>: true in case of success, false in case of fail. 
+     * @return Promise<boolean>: true in case of success, false in case of fail.
      */
     public async checkCredential( cred : Credential ) : Promise<boolean> {
         const url = cred.serverURL + "/app/rest/";
         const p : Promise<boolean> = new Promise((resolve, reject) => {
             this.makeRequest("GET", url, cred)
             .then((response) => { resolve(true); })
-            .catch((err) => { 
+            .catch((err) => {
                 if (err.status === Constants.HTTP_STATUS_UNAUTHORIZED) {
-                    VsCodeUtils.showErrorMessage(Strings.STATUS_CODE_401);     
+                    VsCodeUtils.showErrorMessage(Strings.STATUS_CODE_401);
                 } else {
-                    VsCodeUtils.showErrorMessage(Strings.UNEXPECTED_EXCEPTION);                                        
+                    VsCodeUtils.showErrorMessage(Strings.UNEXPECTED_EXCEPTION);
                 }
-                resolve(false); 
+                resolve(false);
             });
         });
         return p;
     }
-    
     public async getSuitableBuildConfig( tcFormatedFilePaths : string[], cred : Credential ) : Promise<BuildConfig[]> {
-        //TODO: implement with RestBuildConfigResolver class. API from TeamCity required. 
+        //TODO: implement with RestBuildConfigResolver class. API from TeamCity required.
         throw "UnsupportedMethodException.";
     }
 }
-
 
 export class TCXmlRpcApiProvider implements TCApiProvider {
 
