f2e4386a773e86d38371b3dc426a91aa15a7eac5
abdula277 <gripanov@gmail.com>
2017-12-05 15:13:35 +0300

    Remove user credentials when he logout by hands

--- src/bll/commands/signout.ts
+++ src/bll/commands/signout.ts
@@ -4,18 +4,23 @@ import {TeamCityStatusBarItem} from "../../view/teamcitystatusbaritem";
 import {CredentialsStore} from "../credentialsstore/credentialsstore";
 import {inject, injectable} from "inversify";
 import {TYPES} from "../utils/constants";
+import {PersistentStorageManager} from "../credentialsstore/persistentstoragemanager";
 
 @injectable()
 export class SignOut implements Command {
 
     private credentialsStore: CredentialsStore;
+    private persistentStorageManager: PersistentStorageManager;
 
-    public constructor(@inject(TYPES.CredentialsStore) credentialsStore: CredentialsStore) {
+    public constructor(@inject(TYPES.CredentialsStore) credentialsStore: CredentialsStore,
+                       @inject(TYPES.PersistentStorageManager) persistentStorageManager: PersistentStorageManager) {
         this.credentialsStore = credentialsStore;
+        this.persistentStorageManager = persistentStorageManager;
     }
 
     public async exec(): Promise<void> {
         this.credentialsStore.removeCredentials();
         TeamCityStatusBarItem.setLoggedOut();
+        return this.persistentStorageManager.removeCredentials();
     }
 }
