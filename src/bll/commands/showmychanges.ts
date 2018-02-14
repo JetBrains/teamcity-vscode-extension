df9ea4fd5bd1fa1e5901a224ebebbeb97ad87a8d
Grigory Panov <gripanov@gmail.com>
2018-02-14 22:04:03 +0300

    Don't write anything to the output during the ShowMyChanges command

--- src/bll/commands/showmychanges.ts
+++ src/bll/commands/showmychanges.ts
@@ -1,8 +1,6 @@
 import {inject, injectable} from "inversify";
 import {TYPES} from "../utils/constants";
 import {SummaryDao} from "../../dal/summarydao";
-import {Output} from "../../view/output";
-import {MessageConstants} from "../utils/messageconstants";
 import {Change} from "../entities/change";
 import {Logger} from "../utils/logger";
 import {IChangesProvider} from "../../view/dataproviders/interfaces/ichangesprovider";
@@ -12,28 +10,21 @@ import {TimePeriod} from "../entities/timeperiod";
 export class ShowMyChanges implements Command {
 
     private summaryDao: SummaryDao;
-    private output: Output;
     private changesProvider: IChangesProvider;
 
     public constructor(@inject(TYPES.SummaryDao) summaryDao: SummaryDao,
-                       @inject(TYPES.Output) output: Output,
                        @inject(TYPES.ChangesProvider) changesProvider) {
         this.summaryDao = summaryDao;
-        this.output = output;
         this.changesProvider = changesProvider;
     }
 
     public async exec(): Promise<void> {
         Logger.logDebug("ShowMyChanges::exec start");
-        const stringBuilder = [];
-        stringBuilder.push("My Changes:\n");
         const summary = await this.summaryDao.get();
         const changeSet = [summary.changes, summary.personalChanges];
         if ((!summary.changes || summary.changes.length === 0) &&
             (!summary.personalChanges || summary.personalChanges.length === 0)) {
             Logger.logDebug(`ShowMyChanges::exec: personal changes were not found`);
-            stringBuilder.push(MessageConstants.NOTHING_TO_SHOW);
-            stringBuilder.push("----------------------");
             return;
         }
 
@@ -52,46 +43,11 @@ export class ShowMyChanges implements Command {
                 }
             });
         });
+
         const timePeriods = [new TimePeriod("Today", todayChanges), new TimePeriod("Yesterday", yesterdayChanges), new TimePeriod("Older", olderChanges)];
         this.changesProvider.setContent(timePeriods);
 
-        stringBuilder.push("----------------------");
-        stringBuilder.push("Today:");
-        stringBuilder.push("----------------------");
-        stringBuilder.push(this.formTextFromChanges(todayChanges));
-        stringBuilder.push("Yesterday:");
-        stringBuilder.push("----------------------");
-        stringBuilder.push(this.formTextFromChanges(yesterdayChanges));
-        stringBuilder.push("Older:");
-        stringBuilder.push("----------------------");
-        stringBuilder.push(this.formTextFromChanges(olderChanges));
-
-        this.output.appendLine(stringBuilder.join("\n"));
-        this.output.show();
-        Logger.logDebug("ShowMyChanges::exec finished " + stringBuilder.join("\n"));
-    }
-
-    private formTextFromChanges(changes: Change[]) {
-        const stringBuilder = [];
-        if (changes && changes.length !== 0) {
-            Logger.logDebug(`ShowMyChanges::exec: found ${changes.length} personal changes`);
-            changes.forEach((change) => {
-                const appendLine = ShowMyChanges.formAppendLine(change);
-                stringBuilder.push(appendLine);
-                stringBuilder.push("----------------------");
-            });
-        } else {
-            Logger.logDebug(`ShowMyChanges::exec: personal changes were not found`);
-            stringBuilder.push("----------------------");
-        }
-        return stringBuilder.join("\n");
-    }
-
-    private static formAppendLine(change: Change) {
-        const description = change.myDescription ? change.myDescription : "<no comment>";
-        const myVersionControlName = change.myVersionControlName;
-        const changesCount = `${change.myChangesCount} file${change.myChangesCount === 0 ? "s" : ""}`;
-        return `${description}\n${myVersionControlName} | ${changesCount}`;
+        Logger.logDebug("ShowMyChanges::exec finished ");
     }
 
     private isToday(date: Date): boolean {
