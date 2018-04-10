03ce2f2aa794b68f1b01221fd3ace1f1a3cd3802
rugpanov <gripanov@gmail.com>
2018-04-10 11:47:54 +0300

    Fix incorrect integration tests

--- test/dal/weblinklistener.integration.test.ts
+++ test/dal/weblinklistener.integration.test.ts
@@ -7,6 +7,8 @@ import {WindowProxy} from "../../src/bll/moduleproxies/window-proxy";
 import {FsProxy} from "../../src/bll/moduleproxies/fs-proxy";
 import {WebLinkListener} from "../../src/dal/weblinklistener";
 import {anything, instance, mock, when} from "ts-mockito";
+import {Uri, WorkspaceFolder} from "vscode";
+import * as TypeMoq from "typemoq";
 
 suite("WebLinkListener", () => {
     test("should verify sending incorrect request", async function () {
@@ -29,7 +31,7 @@ suite("WebLinkListener", () => {
     });
 
     test("should verify sending correct request", async function () {
-        const incorrectRequest = "Hello, server! Love, Client.";
+        const incorrectRequest = "Hello, server! HTTP/1.1 Client.";
         const correctRequest = "GET /file?file=1.ts&project=&noCache=1523307618951&server=" +
             "http://localhost:8111/bs HTTP/1.1\n" +
             "logger.js:24\n" +
@@ -50,7 +52,7 @@ suite("WebLinkListener", () => {
         }
         for (let port = SOCKET_NUMBER_START; port <= SOCKET_NUMBER_END; port++) {
             correctData = await sendSocketRequest(port, correctRequest);
-            if (correctData) {
+            if (correctData && !areBuffersEqual(correctData, incorrectData)) {
                 break;
             }
         }
@@ -60,7 +62,7 @@ suite("WebLinkListener", () => {
 
         server.dispose();
         if (!receivedCorrectResponse) {
-            throw new Error("We don't received correct response from VSCode.");
+            throw new Error("We haven't received correct response from VSCode.");
         }
     });
 });
@@ -105,10 +107,25 @@ function setupServer(): WebLinkListener {
     const windowProxy = mock(WindowProxy);
     when(windowProxy.showTextDocument(anything())).thenReturn(Promise.resolve(undefined));
     const windowSpy = instance(windowProxy);
+
+    const uriMock: TypeMoq.IMock<Uri> = TypeMoq.Mock.ofType<Uri>();
+    uriMock.setup((foo) => foo.path).returns(() => "path");
+    uriMock.setup((foo) => foo.fsPath).returns(() => "fsPath");
+    const uriSpy: Uri = uriMock.object;
+
+    const workspaceFolderMock: TypeMoq.IMock<WorkspaceFolder> = TypeMoq.Mock.ofType<WorkspaceFolder>();
+    workspaceFolderMock.setup((foo) => foo.uri).returns(() => uriSpy);
+    const workspaceFolderSpy: WorkspaceFolder = workspaceFolderMock.object;
+
     const workspaceProxy = mock(WorkspaceProxy);
-    when(workspaceProxy.getWorkspaceFolders()).thenReturn([]);
+    when(workspaceProxy.getWorkspaceFolders()).thenReturn([workspaceFolderSpy]);
     const workspaceSpy = instance(workspaceProxy);
-    const httpHostRequest: HttpHostRequest = new HttpHostRequest(workspaceSpy, fsProxySpy, new UriProxy(), windowSpy);
+
+    const uriProxyMock = mock(UriProxy);
+    when(uriProxyMock.file(anything())).thenReturn(uriSpy);
+    const uriProxySpy = instance(uriProxyMock);
+
+    const httpHostRequest: HttpHostRequest = new HttpHostRequest(workspaceSpy, fsProxySpy, uriProxySpy, windowSpy);
     return new WebLinkListener(httpHostRequest);
 }
 
