c632dde0f51cda746fb4ac58805ca0268102f1eb
rugpanov <gripanov@gmail.com>
2018-05-25 18:42:04 +0300

    Add injectable annotation for ContextImpl

--- src/view/ContextImpl.ts
+++ src/view/ContextImpl.ts
@@ -1,6 +1,8 @@
 import {Context} from "./Context";
 import {commands} from "vscode";
+import {injectable} from "inversify";
 
+@injectable()
 export class ContextImpl implements Context {
     showPreTestedCommitButton(show: boolean) {
         commands.executeCommand("setContext", "teamcity-show-pretested-commit", show);
