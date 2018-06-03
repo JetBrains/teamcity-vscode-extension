39b0aeba2c1a366586c298a31a6318f8f58e8dc2
rugpanov <gripanov@gmail.com>
2018-06-03 22:48:21 +0300

    Fix SignOut command

--- src/bll/commands/signout.ts
+++ src/bll/commands/signout.ts
@@ -3,6 +3,7 @@ import {CredentialsStore} from "../credentialsstore/credentialsstore";
 import {inject, injectable} from "inversify";
 import {TYPES} from "../utils/constants";
 import {PersistentStorageManager} from "../credentialsstore/persistentstoragemanager";
+import {ProviderManager} from "../../view/providermanager";
 
 @injectable()
 export class SignOut implements Command {
@@ -13,7 +14,8 @@ export class SignOut implements Command {
 
     public constructor(@inject(TYPES.CredentialsStore) credentialsStore: CredentialsStore,
                        @inject(TYPES.PersistentStorageManager) persistentStorageManager: PersistentStorageManager,
-                       @inject(TYPES.TeamCityStatusBarItem) statusBarItem: TeamCityStatusBarItem) {
+                       @inject(TYPES.TeamCityStatusBarItem) statusBarItem: TeamCityStatusBarItem,
+                       @inject(TYPES.ProviderManager) private readonly providerManager: ProviderManager) {
         this.credentialsStore = credentialsStore;
         this.persistentStorageManager = persistentStorageManager;
         this.statusBarItem = statusBarItem;
@@ -22,6 +24,7 @@ export class SignOut implements Command {
     public async exec(): Promise<void> {
         this.credentialsStore.removeCredentials();
         this.statusBarItem.setLoggedOut();
+        this.providerManager.showEmptyDataProvider();
         return this.persistentStorageManager.removeCredentials();
     }
 }
