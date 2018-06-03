fccbca6962a60bad34fa467c825c6e67c4cf3ff8
rugpanov <gripanov@gmail.com>
2018-06-03 18:34:24 +0300

    Simplify a bit ChangeItem

--- src/bll/entities/presentable/changeitem.ts
+++ src/bll/entities/presentable/changeitem.ts
@@ -5,11 +5,8 @@ import {ImageConstants} from "../imageconstants";
 
 export class ChangeItem extends TreeItem {
 
-    private readonly change: Change;
-
-    constructor(change: Change) {
+    constructor(private readonly change: Change) {
         super(Utils.formChangeLabel(change).replace("\n", " | "), TreeItemCollapsibleState.None);
-        this.change = change;
     }
 
     public get iconPath(): string | Uri | { light: string | Uri; dark: string | Uri } {
