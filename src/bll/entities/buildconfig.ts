6c08bc0594eda4f60890e68a737c64900f82b86a
rugpanov <gripanov@gmail.com>
2018-06-03 15:16:00 +0300

    Add customization support for buildConfig: queueAtTop and parameters

--- src/bll/entities/buildconfig.ts
+++ src/bll/entities/buildconfig.ts
@@ -1,11 +1,58 @@
-export class BuildConfig {
-    public readonly id: string;
-    public readonly externalId: string;
-    public readonly name: string;
+import {ParameterItem} from "./presentable/ParameterItem";
+import {ParameterType} from "../utils/constants";
 
-    constructor(id: string, externalId: string, name: string) {
-        this.id = id;
-        this.externalId = externalId;
-        this.name = name;
+export class BuildConfig {
+
+    private queueAtTop: boolean = false;
+    private parameters: ParameterItem[][] = [];
+
+    constructor(public readonly id: string, public readonly externalId: string, public readonly name: string) {
+        //
+    }
+
+    public invertQueueAtTop(): void {
+        this.queueAtTop = !this.queueAtTop;
+    }
+
+    public shouldQueueAtTop(): boolean {
+        return this.queueAtTop;
+    }
+
+    public addConfigParameters(param: ParameterItem) {
+        this.addParameter(param, ParameterType.ConfigParameter);
+    }
+
+    public getConfigParameters(): ParameterItem[] {
+        return this.getParameter(ParameterType.ConfigParameter);
+    }
+
+    public addSystemProperties(param: ParameterItem) {
+        this.addParameter(param, ParameterType.SystemProperty);
+    }
+
+    public getSystemProperties(): ParameterItem[] {
+        return this.getParameter(ParameterType.SystemProperty);
+    }
+
+    public addEnvVariables(param: ParameterItem) {
+        this.addParameter(param, ParameterType.EnvVariable);
+    }
+
+    public getEnvVariables(): ParameterItem[] {
+        return this.getParameter(ParameterType.EnvVariable);
+    }
+
+    private addParameter(param: ParameterItem, paramType: ParameterType) {
+        if (!this.parameters[paramType]) {
+            this.parameters[paramType] = [];
+        }
+        this.parameters[paramType].push(param);
+    }
+
+    private getParameter(paramType: ParameterType) {
+        if (!this.parameters[paramType]) {
+            return [];
+        }
+        return this.parameters[paramType];
     }
 }
