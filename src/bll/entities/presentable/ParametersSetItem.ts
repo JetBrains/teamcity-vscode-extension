bf4b76af8f96279d3518403c80be3fbeb45fc6e5
rugpanov <gripanov@gmail.com>
2018-06-10 12:36:05 +0300

    Remove icons for config sets as redundant

--- src/bll/entities/presentable/ParametersSetItem.ts
+++ src/bll/entities/presentable/ParametersSetItem.ts
@@ -1,7 +1,6 @@
 import {ExpandableItem} from "./expandableitem";
-import {TreeItemCollapsibleState, Uri} from "vscode";
+import {TreeItemCollapsibleState} from "vscode";
 import {Parameter} from "../Parameter";
-import * as path from "path";
 import {ParameterItem} from "./ParameterItem";
 
 export class ParametersSetItem extends ExpandableItem {
@@ -13,13 +12,5 @@ export class ParametersSetItem extends ExpandableItem {
         params.forEach((param) => {
             this.children.push(new ParameterItem(param));
         });
-    }
-
-    public get iconPath(): string | Uri | { light: string | Uri; dark: string | Uri } {
-        const iconName: string = "project.svg";
-        return {
-            light: path.join(__dirname, "..", "..", "..", "..", "..", "resources", "icons", "light", iconName),
-            dark: path.join(__dirname, "..", "..", "..", "..", "..", "resources", "icons", "dark", iconName)
-        };
     }
 }
