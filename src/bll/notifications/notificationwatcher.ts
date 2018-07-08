ef4730acd3694910d6d4ded2374e7a79124cb592
rugpanov <gripanov@gmail.com>
2018-07-08 22:55:24 +0300

    Remove redundant imports

--- src/bll/notifications/notificationwatcher.ts
+++ src/bll/notifications/notificationwatcher.ts
@@ -1,6 +1,4 @@
 import {Disposable} from "vscode";
-import {TeamCityOutput} from "../../view/teamcityoutput";
-import {CredentialsStore} from "../credentialsstore/credentialsstore";
 
 export interface NotificationWatcher extends Disposable {
     activate(): Promise<void>;
